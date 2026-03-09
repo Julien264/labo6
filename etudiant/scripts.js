766dd674976a097b8b2792a8f460fabe52616696
Julien264 <deajulien2005@gmail.com>
2026-03-09 19:59:25 -0400

    fini

--- etudiant/scripts.js
+++ etudiant/scripts.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('.button');
+const modal = document.querySelector('.modal');
 
 for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
@@ -7,5 +8,19 @@ for (let i = 0; i < buttons.length; i++) {
 }
 
 function openModal(){
-    console.log("allo");
+    const path = this.previousElementSibling.src;
+    const image = document.createElement('img');
+    image.src = path;
+    image.classList.add('zoom');
+
+    modal.appendChild(image);
+    modal.classList.add('visible');
+
+    // Fermeture de la modale
+    image.addEventListener('click', closeModal);
+}
+
+function closeModal(event){
+    modal.classList.remove("visible");
+    modal.removeChild(event.currentTarget);
 }
